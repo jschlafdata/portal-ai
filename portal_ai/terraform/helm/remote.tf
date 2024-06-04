0bc030e5a4ef01bc2e5352e99673c4218782f330
john@schlafdata.com <jschlafdata>
2024-06-04 09:57:17 -0600

    removing pushed configs

--- portal_ai/terraform/helm/remote.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-            backend "s3" {
-                bucket = "tfstate-dev.principalcg.cloud"
-                key = "helm"
-                region = "us-west-2"
-                encrypt = true
-                }
-            }
