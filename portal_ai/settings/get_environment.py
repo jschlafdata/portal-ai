a5fda4d5a9e18453abf6a842bae71b8f2767c011
john@schlafdata.com <jschlafdata>
2024-06-04 10:00:47 -0600

    purge and refresh

--- portal_ai/settings/get_environment.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def get_project_env():
-    environment = os.getenv('deployment-environment', 'dev')
-    return environment
--- portal_ai/terraform/aws_base/providers.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = local.global_settings.aws_region
-}
